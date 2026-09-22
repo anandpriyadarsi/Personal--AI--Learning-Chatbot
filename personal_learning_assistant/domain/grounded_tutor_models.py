51d21a1da466561fc120014e08551cb27c89575d
Anand Priyadarsi <anandpriyadarsi883@gmail.com>
2026-09-23 00:25:50 +0530

    feat: carry Tutor 2.2 persistent student model in grounding plan

--- personal_learning_assistant/domain/grounded_tutor_models.py
+++ personal_learning_assistant/domain/grounded_tutor_models.py
@@ -23,6 +23,7 @@ class GroundingPlan:
     teaching_intent: str = ""
     teaching_instruction: str = ""
     adaptive_state: Mapping[str, object] = field(default_factory=dict)
+    persistent_student_model: Mapping[str, object] = field(default_factory=dict)
     retrieval_queries: Tuple[str, ...] = ()
 
 
