072847a7c9b7007ed30a023fb53a6f0a5152ec84
Anand Priyadarsi <anandpriyadarsi883@gmail.com>
2026-09-23 12:07:33 +0530

    feat: carry Tutor 2.3 goal and teaching plan in grounding contract

--- personal_learning_assistant/domain/grounded_tutor_models.py
+++ personal_learning_assistant/domain/grounded_tutor_models.py
@@ -23,6 +23,8 @@ class GroundingPlan:
     teaching_intent: str = ""
     teaching_instruction: str = ""
     adaptive_state: Mapping[str, object] = field(default_factory=dict)
+    session_goal: Mapping[str, object] = field(default_factory=dict)
+    teaching_plan: Mapping[str, object] = field(default_factory=dict)
     persistent_student_model: Mapping[str, object] = field(default_factory=dict)
     teaching_policy: Mapping[str, object] = field(default_factory=dict)
     retrieval_queries: Tuple[str, ...] = ()
